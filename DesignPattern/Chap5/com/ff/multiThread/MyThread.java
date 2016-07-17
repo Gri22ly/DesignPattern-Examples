81f030ed0adc1f0cc663ef945778522adc984be8
giraphadoop <178246054@qq.com>
2016-07-17 11:17:09 -0400

    Supplemental Code for multi-thread

---
+++ DesignPattern/Chap5/com/ff/multiThread/MyThread.java
@@ -0,0 +1,16 @@
+package com.ff.multiThread;
+
+/*
+ * Inherit Thread Class to implement multi-threads
+ * 
+ */
+
+public class MyThread extends Thread{
+	
+	private String name;
+	
+	public MyThread(String name) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
