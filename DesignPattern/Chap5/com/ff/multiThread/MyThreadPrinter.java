0a5755ca6c205b75275a8d72ff4f7f2f0bb9db84
Gri22ly <178246054@qq.com>
2016-07-28 01:53:49 -0400

    July 28

---
+++ DesignPattern/Chap5/com/ff/multiThread/MyThreadPrinter.java
@@ -0,0 +1,17 @@
+package com.ff.multiThread;
+
+/**
+ * 
+ * @author FEIFAN
+ *
+ * Print A B C
+ */
+public class MyThreadPrinter implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
