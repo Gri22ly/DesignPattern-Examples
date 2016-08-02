ffaa232f722d8ce2cff4b7af8c98e0970abfdbbe
giraphadoop <178246054@qq.com>
2016-08-02 16:47:22 -0400

    none

---
+++ DesignPattern/Chap5/com/ff/multiThread/MyThread.java
@@ -0,0 +1,28 @@
+package com.ff.multiThread;
+
+/*
+ * Inherit Thread Class to implement multi-threads
+ * 
+ */
+
+public class MyThread extends Thread{
+	
+	private String name;
+
+	@Override
+	public void run() {
+		for(int i = 0; i < 5; i++) {
+			System.out.println(name + " thread prints  " + i);
+			try {
+				sleep((int) Math.random() * 10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public MyThread(String name) {
+		this.name = name;
+	}
+
+}
